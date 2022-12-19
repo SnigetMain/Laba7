7025547d575d0822b5a3e33aa5186dac41a27d69
kutwi <s.volkov@uniyar.ac.ru>
2022-12-19 19:44:54 +0300

    Сортировка QuickSort
    от Сереги

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -6,23 +6,51 @@
 using namespace std;
 using namespace std::chrono;
 
-long long A[10000000];
+long long Array[10000000];
+long long Array_Elements = 10000000;// размер массива
 long double SrTimer = 0;
-string OutputData = "output.txt"; // Задать имя файла
+string OutputData = "QuickSortTime.txt"; // Задать имя файла
 
 void randomArray() {
-	for (long long i = 0; i < 10000000;i++) {
-		A[i] = rand() % 10000000;
+	for (long long i = 0; i < Array_Elements;i++) {
+		Array[i] = rand() % Array_Elements;
 	}
 }
+void qsortRecursive(long long* Array, long long size) {
+    int i = 0;
+    int j = size - 1;
+    int mid = Array[size / 2];
+    do {
+        while (Array[i] < mid) {
+            i++;
+        }
+        while (Array[j] > mid) {
+            j--;
+        }
+        if (i <= j) {
+            int tmp = Array[i];
+            Array[i] = Array[j];
+            Array[j] = tmp;
 
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    if (j > 0) {
+        qsortRecursive(Array, j + 1);
+    }
+    if (i < size) {
+        qsortRecursive(&Array[i], size - i);
+    }
+}
 
 void Tester(string NameSort,int i) {
 	ofstream file(OutputData, ios_base::app);
 	randomArray();
 	auto start = high_resolution_clock::now();
 	// место для функции сортировки
-
+    
+    qsortRecursive(Array, Array_Elements);
 	//............................
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
@@ -34,9 +62,9 @@ void Tester(string NameSort,int i) {
 
 int main() {
 	srand(time(0));
-	int NumOfTest = 10;// кол-во тестов
+	int NumOfTest = 1000;// кол-во тестов
 	for (int i = 0; i < NumOfTest; i++) {
-		Tester("QuickSort",i);
+		Tester("qsort",i);// изменить только имя сортировки 
 	}
 	ofstream file(OutputData, ios_base::app);
 	file << "Среднее время :" << SrTimer / NumOfTest;
