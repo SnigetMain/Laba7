f1163c931ac1ec3314cd94b60b1149785f470197
SnigetMain <v.gontar@uniyar.ac.ru>
2022-12-21 02:25:03 +0300

    HeapSort

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono;
 long long Array[10000000];
 long long Array_Elements = 10000000;// размер массива
 long double SrTimer = 0;
-string OutputData = "output.txt"; // Задать имя файла
+string OutputData = "HeapSortTime.txt"; // Задать имя файла
 
 void randomArray() {
 	for (long long i = 0; i < Array_Elements;i++) {
@@ -17,13 +17,48 @@ void randomArray() {
 	}
 }
 
+void heapify(int N,int i)
+{
+
+    int largest = i;
+
+    int l = 2 * i + 1;
+
+    int r = 2 * i + 2;
+
+    if (l < N && Array[l] > Array[largest])
+        largest = l;
+
+    if (r < N && Array[r] > Array[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(Array[i], Array[largest]);
+
+        heapify(N, largest);
+    }
+}
+
+void heapSort()
+{
+
+    for (int i = Array_Elements / 2 - 1; i >= 0; i--)
+        heapify(Array_Elements, i);
+
+    for (int i = Array_Elements - 1; i > 0; i--) {
+
+        swap(Array[0], Array[i]);
+
+        heapify(i, 0);
+    }
+}
 
 void Tester(string NameSort,int i) {
 	ofstream file(OutputData, ios_base::app);
 	randomArray();
 	auto start = high_resolution_clock::now();
 	// место для функции сортировки
-
+	heapSort();
 	//............................
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
@@ -35,9 +70,9 @@ void Tester(string NameSort,int i) {
 
 int main() {
 	srand(time(0));
-	int NumOfTest = 10;// кол-во тестов
+	int NumOfTest = 1000;// кол-во тестов
 	for (int i = 0; i < NumOfTest; i++) {
-		Tester("QuickSort",i);
+		Tester("HeapSort",i);
 	}
 	ofstream file(OutputData, ios_base::app);
 	file << "Average Time :" << SrTimer / NumOfTest;
