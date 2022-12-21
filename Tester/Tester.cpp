024b804b393ad6a12ac2df53116afe2ad7413aca
SnigetMain <v.gontar@uniyar.ac.ru>
2022-12-21 12:40:41 +0300

    Уменьшенные тесты вставок

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono;
 long long Array[10000000];
 long long Array_Elements = 10000000;// размер массива
 long double SrTimer = 0;
-string OutputData = "output.txt"; // Задать имя файла
+string OutputData = "insertionSortеTime.txt"; // Задать имя файла
 
 void randomArray() {
 	for (long long i = 0; i < Array_Elements;i++) {
@@ -17,17 +17,17 @@ void randomArray() {
 	}
 }
 
-void insertionSort(int data[], int lenD)
+void insertionSort()
 {
 	int key = 0;
 	int i = 0;
-	for (int j = 1;j < lenD;j++) {
-		key = data[j];
+	for (int j = 1;j < Array_Elements;j++) {
+		key = Array[j];
 		i = j - 1;
-		while (i >= 0 && data[i] > key) {
-			data[i + 1] = data[i];
+		while (i >= 0 && Array[i] > key) {
+			Array[i + 1] = Array[i];
 			i = i - 1;
-			data[i + 1] = key;
+			Array[i + 1] = key;
 		}
 	}
 }
@@ -37,7 +37,7 @@ void Tester(string NameSort,int i) {
 	randomArray();
 	auto start = high_resolution_clock::now();
 	// место для функции сортировки
-
+	insertionSort();
 	//............................
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
@@ -49,9 +49,9 @@ void Tester(string NameSort,int i) {
 
 int main() {
 	srand(time(0));
-	int NumOfTest = 10;// кол-во тестов
+	int NumOfTest = 1000;// кол-во тестов
 	for (int i = 0; i < NumOfTest; i++) {
-		Tester("QuickSort",i);
+		Tester("insertionSort",i);
 	}
 	ofstream file(OutputData, ios_base::app);
 	file << "Average Time :" << SrTimer / NumOfTest;
