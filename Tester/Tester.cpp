dddfd2e57a2c4d694d5123d71ecd33128bd39b00
SnigetMain <v.gontar@uniyar.ac.ru>
2022-12-21 10:31:04 +0300

    Тесты

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -9,7 +9,10 @@ using namespace std::chrono;
 long long Array[10000000];
 long long Array_Elements = 10000000;// размер массива
 long double SrTimer = 0;
-string OutputData = "output.txt"; // Задать имя файла
+string OutputData = "MergeSortTime.txt"; // Задать имя файла
+
+
+int* mas = new int[10000000];
 
 void randomArray() {
 	for (long long i = 0; i < Array_Elements;i++) {
@@ -17,34 +20,33 @@ void randomArray() {
 	}
 }
 
-void Merge(int* A, int first, int last)
+void Merge(int first, int last)
 {
     int middle, start, final, j;
-    int* mas = new int[10000000];
     middle = (first + last) / 2;
     start = first;
     final = middle + 1;
     for (j = first; j <= last; j++)
-        if ((start <= middle) && ((final > last) || (A[start] < A[final])))
+        if ((start <= middle) && ((final > last) || (Array[start] < Array[final])))
         {
-            mas[j] = A[start];
+            mas[j] = Array[start];
             start++;
         }
         else
         {
-            mas[j] = A[final];
+            mas[j] = Array[final];
             final++;
         }
-    for (j = first; j <= last; j++) A[j] = mas[j];
-    delete[]mas;
+    for (j = first; j <= last; j++) Array[j] = mas[j];
+    //delete[]mas;
 }
-void MergeSort(int* A, int first, int last)
+void MergeSort(int first, int last)
 {
     if (first < last)
     {
-        MergeSort(A, first, (first + last) / 2);
-        MergeSort(A, (first + last) / 2 + 1, last);
-        Merge(A, first, last);
+        MergeSort(first, (first + last) / 2);
+        MergeSort((first + last) / 2 + 1, last);
+        Merge(first, last);
     }
 }
 void Tester(string NameSort,int i) {
@@ -52,7 +54,7 @@ void Tester(string NameSort,int i) {
 	randomArray();
 	auto start = high_resolution_clock::now();
 	// место для функции сортировки
-
+    MergeSort(0, Array_Elements);
 	//............................
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
@@ -64,9 +66,9 @@ void Tester(string NameSort,int i) {
 
 int main() {
 	srand(time(0));
-	int NumOfTest = 10;// кол-во тестов
+	int NumOfTest = 1000;// кол-во тестов
 	for (int i = 0; i < NumOfTest; i++) {
-		Tester("QuickSort",i);
+		Tester("MergeSort",i);
 	}
 	ofstream file(OutputData, ios_base::app);
 	file << "Average Time :" << SrTimer / NumOfTest;
